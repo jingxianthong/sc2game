0ac20dd353c09e82fd453708f4162b793365a386
jingxianthong <77329585+jingxianthong@users.noreply.github.com>
2022-05-05 20:06:13 +0800

    Add files via upload

--- my_bot.py
+++ my_bot.py
@@ -1,13 +1,16 @@
 import sc2
 from sc2.bot_ai import BotAI
 from sc2.player import Bot, Computer
+from sc2.main import run_game
+from sc2.data import Difficulty, Race
+
 
 class MyBot(BotAI):
     async def on_step(self, iteraton: int):
         print(f"This is my bot in literation {iteraton}!")
 
-sc2.run_game(
-    sc2.maps.get("AcropolisLE"),
-    [Bot(sc2.Race.Zerg, MyBot()), Computer(sc2.Race.Zerg, sc2.Difficulty.Hard)],
+run_game(
+    sc2.maps.get("BelShirVestigeLE"),
+    [Bot(Race.Zerg, MyBot()), Computer(Race.Zerg, Difficulty.Hard)],
     realtime= False,
 )
